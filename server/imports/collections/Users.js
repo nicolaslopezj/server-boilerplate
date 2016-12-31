cdfc56514c488ff76e0d351a5a3af5b8863d4b2d
Nicolás López <nicolaslopezj@me.com>
2016-12-31 13:20:16 -0300

    accounts & simple schema

--- server/imports/collections/Users.js
+++ server/imports/collections/Users.js
@@ -1,3 +1,51 @@
 import {Meteor} from 'meteor/meteor'
+import SimpleSchema from 'simpl-schema'
+
+const ProfileSchema = new SimpleSchema({
+  name: {
+    type: String,
+    optional: true
+  }
+})
+
+Meteor.users.attachSchema(new SimpleSchema({
+  emails: {
+    type: Array
+  },
+  'emails.$': {
+    type: Object
+  },
+  'emails.$.address': {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email
+  },
+  'emails.$.verified': {
+    type: Boolean
+  },
+  createdAt: {
+    type: Date
+  },
+  profile: {
+    type: ProfileSchema,
+    optional: true
+  },
+  services: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  },
+  roles: {
+    type: Array,
+    optional: true
+  },
+  'roles.$': {
+    type: String,
+    optional: true
+  },
+  heartbeat: {
+    type: Date,
+    optional: true
+  }
+}))
 
 export default Meteor.users
