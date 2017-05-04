779f2e679bc63741ae269ee841c459a680d06be4
Nicolás López Jullian <nicolaslopezj@me.com>
2017-05-04 12:29:56 -0300

    Update send.js

--- server/imports/emails/send.js
+++ server/imports/emails/send.js
@@ -5,37 +5,33 @@ import {Email} from 'meteor/email'
 const isProduction = process.env.NODE_ENV === 'production'
 const from = process.env.MAIL_FROM || 'test@orionsoft.io' // send emails from
 
-const sendEmail = function ({sync, to, subject, template, data}) {
+const sendEmail = function ({to, subject, template, data}) {
   const html = getContent(template, data)
 
   if (!isProduction) {
+    const oldTo = to
     to = 'test@orionsoft.io'
-    console.log(`Sending testing email to: "${to}"...`)
-    console.log(`with: "${data}"...`)
+    console.log(`Sending testing email to "${to}" instead if "${oldTo}..."`)
   }
-  
+
   const email = {to, from, subject, html}
 
   try {
-    if (sync) {
-      Email.send(email)
-    } else {
-      Meteor.defer(() => Email.send(email))
-    }
+    Email.send(email)
   } catch (error) {
     console.log(`Error sending email to ${to}`, error)
   }
 }
 
-export default function ({sync, usersIds, subject, template, data, addresses}) {
+export default async function ({usersIds, subject, template, data, addresses}) {
   Meteor.users.find({_id: {$in: usersIds}}).forEach(user => {
     const finalData = {...data, user}
     const to = user.emails[0].address
-    sendEmail({sync, to, subject, template, data: finalData})
+    sendEmail({to, subject, template, data: finalData})
   })
 
   if (!addresses) return
   addresses.map(to => {
-    sendEmail({sync, to, subject, template, data})
+    sendEmail({to, subject, template, data})
   })
 }
